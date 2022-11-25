b7270cdadcea8fe0c2849741690a981df5332a44
gitmsx <gitmsx@mail.ru>
2022-11-25 17:36:44 +0300

    Saved on 25.11.2022-17:36:43,92

---
+++ Assets/Scripts/AudioLibMp3.cs
@@ -0,0 +1,21 @@
+using System;
+using System.IO;
+using System.Linq;
+using Random = System.Random;
+
+public class AudioLibMp3
+{
+    public string[] Mp3FilesToArray(string PathToFolder)
+    {
+        string[] allfoldersTmp = Directory.GetFiles(PathToFolder, "*.mp3", SearchOption.AllDirectories);
+        string[] allfolders_mp3 = new string[allfoldersTmp.Length];
+
+        var rnd2 = new Random();
+        int[] a = Enumerable.Range(0, allfoldersTmp.Length).OrderBy(num => rnd2.Next()).ToArray();
+
+        for (int i = 0; i < allfoldersTmp.Length; i++)
+            allfolders_mp3[i] = allfoldersTmp[a[i]];
+        return allfolders_mp3;
+    }
+
+}
---
+++ Assets/Scripts/StartAudioScript.cs
@@ -0,0 +1,83 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class StartAudioScript : MonoBehaviour
+{
+
+    AudioClip _audioClip;
+    AudioSource audioSource;
+    int CurrentClip = 0;
+
+    DateTime timeStart;
+    DateTime timeEnd;
+
+
+
+    string[] ListAllMp3;
+     [SerializeField] float TimeToChangeMp3 = 7;
+
+
+    [SerializeField] AudioClip flySound;
+
+    void Start()
+    {
+        int CurrentClip = 0;
+
+        timeEnd = DateTime.Now;
+        audioSource = GetComponent<AudioSource>();
+        AudioLibMp3 audioLibMp3 = new AudioLibMp3();
+        string PathToFolderMp3 = "I:\\data1\\mp3.eng\\HarryPotter";
+        ListAllMp3 = audioLibMp3.Mp3FilesToArray(PathToFolderMp3);
+
+        StartCoroutine(ListPlay());
+
+
+
+    }
+
+
+
+    IEnumerator ListPlay()
+    {
+
+
+        double dellta = (timeEnd - DateTime.Now).TotalSeconds;
+        Debug.Log(" ListPlay Seconds " + dellta.ToString() + " timeEnd > DateTime.Now " + (timeEnd > DateTime.Now).ToString());
+
+        while (timeEnd > DateTime.Now)
+            yield return new WaitForSeconds((float)dellta);
+        StartCoroutine(LoadAudioClip(ListAllMp3[CurrentClip++]));
+
+    }
+
+
+
+
+    IEnumerator LoadAudioClip(string PtsPath)
+    {
+
+        using (UnityWebRequest uwr = UnityWebRequestMultimedia.GetAudioClip((PtsPath), AudioType.MPEG))
+        {
+            yield return uwr.SendWebRequest();
+            _audioClip = DownloadHandlerAudioClip.GetContent(uwr);
+            audioSource.clip = _audioClip;
+            audioSource.Play();
+            timeStart = DateTime.Now;
+            timeEnd = timeStart.AddSeconds(_audioClip.length);
+            if (TimeToChangeMp3>0)  // fast change for debug and etc
+            timeEnd = timeStart.AddSeconds(TimeToChangeMp3);
+            Debug.Log("TotalSeconds to change track = " + (timeEnd - DateTime.Now).TotalSeconds);
+            StartCoroutine(ListPlay());
+
+        }
+    }
+
+
+}
+
+
+
